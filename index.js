2a3b6ac9b0fee57a3b4e767435105a3794bc6a24
Luis Lozano <lslozano11@gmail.com>
2020-07-03 13:43:58 -0500

    Starting setting ready.
    Title changes and selects a random button. Also the corresponding sound plays.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const buttonsID = ["green", "red", "yellow", "blue"];
+const randomID = Math.round(Math.random() * 3);
+
 $("body").keydown(function(event) {
   if (event.key == "a") {
-    $("h1").text("Level 1")
+    $("h1").text("Level 1");
+    $(`#${buttonsID[randomID]}`).fadeOut(100).fadeIn(100);
+    const buttonSound = new Audio();
+    buttonSound.src = `sounds/${buttonsID[randomID]}.mp3`;
+    buttonSound.play();
   }
 })
+
+
